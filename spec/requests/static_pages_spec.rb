a1e24bcff456982d17f4f6598273c3d3f0448be1
shmilyhh <xh19920904@hotmail.com>
2013-11-11 20:57:22 +0800

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,36 +1,64 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
+
+  subject { page }
+
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-    it "should have the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    before { visit root_path }
+
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem")
+        FactoryGirl.create(:micropost, user: user, content: "Ipsum")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
     end
   end
-  describe "Helper page" do
-	
-	it "should have the content 'Help'" do
-		visit '/static_pages/help'
-		expect(page).to have_content('Help')
-	end
-	it "should have the right title" do
-  		visit '/static_pages/help'
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Helpe")
-	end
-    end
-  describe "About page" do
-	it "should have the content 'About  Us'" do
-		visit '/static_pages/about'
-		expect(page).to have_content('About Us')
-	end
-	it "should have the right title" do
-  		visit '/static_pages/about'
-  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
-	end
-    end
-end
 
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
+
+
+end
