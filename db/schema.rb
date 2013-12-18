7d1329906c60f3bf11e09feb492aa5dcb7aabcb7
shmilyhh <xh19920904@hotmail.com>
2013-12-18 15:46:33 +0800

    Add user following

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216015702) do
+ActiveRecord::Schema.define(version: 20131217110554) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 20131216015702) do
   end
 
   add_index "microposts", ["created_at", "user_id"], name: "index_microposts_on_created_at_and_user_id"
+
+  create_table "relationships", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "relationships", ["followed_id", "follower_id"], name: "index_relationships_on_followed_id_and_follower_id", unique: true
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
